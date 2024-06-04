3130b93913946d76b5a223b20ac71c85cb21a3a5
hasnain242 <78303526+hasnain242@users.noreply.github.com>
2024-06-05 04:41:27 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+document.write('a. A heading stating “Rules for naming JS variables”'+"<br/>")
+document.write('b. Variable names can only contain ______, ______,'+"<br/>")
+document.write('c.______ and ______.'+"<br/>")
+document.write('d. For example $my_1stVariable.'+"<br/>")
+document.write('e. Variables must begin with a ______, ______ or'+"<br/>")
+document.write('f. _____. For example $name, _name or name'+"<br/>")
+document.write('g. Variable names are case _________'+"<br/>")
+document.write('h. Variable names should not be JS _________'+"<br/>")
